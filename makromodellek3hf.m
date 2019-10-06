6e6f2b193ff906716e92a222733cc41a4194de0d
UnidentifiedUniqueUsername <55949898+UnidentifiedUniqueUsername@users.noreply.github.com>
2019-10-06 21:03:14 +0200

    Update makromodellek3hf.m

--- makromodellek3hf.m
+++ makromodellek3hf.m
@@ -6,10 +6,10 @@
 % intervallumát hasraütésszerűen választottam. A piros csillag jelzi a
 % háziban kijött eredményt.
 
-phi = 0:0.25:15;
-L = ((1.09^-0.79 * 0.68) ./ phi).^(1 / (1.76-2*0.68+1.79));
+pszi = 0:0.25:15;
+L = ((1.09^-0.79 * 0.68) ./ pszi).^(1 / (1.76-2*0.68+1.79));
 
 hold on
-plot(L, phi)
+plot(L, pszi)
 plot(0.3953, 4.85, 'r*')
 hold off
