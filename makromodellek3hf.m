f0d609ae450c722c5809d4194908dd53b0c7a9d4
UnidentifiedUniqueUsername <55949898+UnidentifiedUniqueUsername@users.noreply.github.com>
2019-10-06 17:41:33 +0200

    Makormodellek 3. házi feladat

---
+++ makromodellek3hf.m
@@ -0,0 +1,15 @@
+% Makromodellek 3. házi - A9WYT0
+% Különbözõ phi paraméterek mellett kiszámítja, hogy mekkora lesz a
+% foglalkkoztatottak száma.
+%
+% Megjegyzés: az L-re a képlet az elõzõ részfeladatokból következik. A phi
+% intervallumát hasraütésszerûen választottam. A piros csillag jelzi a
+% háziban kijött eredményt.
+
+phi = 0:0.25:15;
+L = ((1.09^-0.79 * 0.68) ./ phi).^(1 / (1.76-2*0.68+1.79));
+
+hold on
+plot(L, phi)
+plot(0.3953, 4.85, 'r*')
+hold off
